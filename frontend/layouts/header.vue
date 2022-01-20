c5f470a72022d8078bc77c81cf31d002edad1d3e
takuya86 <takuya86takuya@gmail.com>
2022-01-21 00:17:11 +0900

    サイドバー追加

--- frontend/layouts/header.vue
+++ frontend/layouts/header.vue
@@ -1,22 +1,39 @@
 <template>
-  <v-app-bar app dark>
-    <appLogo />
-    <v-toolbar-title>
-      {{ appName }}
-    </v-toolbar-title>
-  </v-app-bar>
+  <header>
+    <v-app-bar app dark>
+      <appLogo />
+      <v-toolbar-title>
+        {{ appName }}
+      </v-toolbar-title>
+      <v-spacer />
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" fixed temporary right>
+      <v-list nav dense>
+        <v-list-item-group>
+          <v-list-item>
+            <v-list-item-title>HOME</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>ABOUT</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </header>
 </template>
 
 <script>
 import appLogo from '~/components/ui/appLogo'
 export default {
+  components: {
+    appLogo,
+  },
   data({ $config: { appName } }) {
     return {
       appName,
+      drawer: false,
     }
-  },
-  components: {
-    appLogo,
   },
 }
 </script>
