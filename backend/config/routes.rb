964597f0aa15078fd4647d7677b5d360846a97bc
takuya86 <takuya86takuya@gmail.com>
2022-01-27 23:57:45 +0900

    ルーティング追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        sessions: "api/v1/auth/sessions",
+        registrations: 'api/v1/auth/registrations',
+        omniauth_callbacks: "api/v1/auth/omniauth_callbacks",
+        confirmations: "api/v1/auth/confirmations",
+        passwords: "api/v1/auth/passwords",
+      }
+    end
+  end
 end
