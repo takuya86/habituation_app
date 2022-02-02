b0da22d346c015500004bf451f01d162c237181c
takuya86 <takuya86takuya@gmail.com>
2022-02-02 23:58:20 +0900

    新規登録実装中

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5000" # Nuxt側はポート番号3000で作るので「localhost:3000」を指定
-    resource "*",
+    origins ['http://localhost:5000']
+
+    resource '*',
       headers: :any,
-      expose: ["access-token", "expiry", "token-type", "uid", "client"], # 追記
+      :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,13 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :user
-      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        sessions: "api/v1/auth/sessions",
-        registrations: 'api/v1/auth/registrations',
-        omniauth_callbacks: "api/v1/auth/omniauth_callbacks",
-        confirmations: "api/v1/auth/confirmations",
-        passwords: "api/v1/auth/passwords",
-      }
+      mount_devise_token_auth_for 'User', at: 'auth'
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
     end
   end
 end
